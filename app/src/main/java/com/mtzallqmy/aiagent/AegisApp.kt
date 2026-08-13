288954965c1a441eed75f696589dfb3290a7dd3e
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-14 00:27:31 +0300

    fix(app): restore coroutine launch import

--- app/src/main/java/com/mtzallqmy/aiagent/AegisApp.kt
+++ app/src/main/java/com/mtzallqmy/aiagent/AegisApp.kt
@@ -61,6 +61,7 @@ import java.io.File
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class AegisApp : Application(), ScheduleRuntimeOwner, ScheduleExecutionHost {
     private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
