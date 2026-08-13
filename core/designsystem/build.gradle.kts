321082ca0aa4fb1ab1d4b5da200aeaecd051bc32
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-14 00:51:09 +0300

    ui: add shared design system module

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
+}
+
+android {
+    namespace = "com.mtzallqmy.aiagent.designsystem"
+    compileSdk = 34
+
+    defaultConfig { minSdk = 26 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
+}
+
+dependencies {
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material.icons)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+}
