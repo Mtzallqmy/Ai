c1d72d61cbff0b2fd0e433b0c2d1b8257a4c8ebf
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-14 03:17:50 +0300

    fix(ci): inherit benchmark plugin versions from root

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.test") version "8.5.2"
-    id("org.jetbrains.kotlin.android") version "1.9.24"
+    id("com.android.test")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
